193fa911b4849d3db75ba6b83ab1032e507949d6
jusang53 <98146496+jusang53@users.noreply.github.com>
2023-03-21 19:03:41 +0900

    Add files via upload

---
+++ Send_Report.m
@@ -0,0 +1,12 @@
+setpref('Internet','E_mail',mail);
+% setpref('Internet','SMTP_Server','smtp.naver.com');
+setpref('Internet','SMTP_Server','smtp.gmail.com');
+setpref('Internet','SMTP_Username',mail);
+setpref('Internet','SMTP_Password',password);
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+
+% To. Title. Body. File
+sendmail(To_mail,mail_title,mail_body,mail_file);
